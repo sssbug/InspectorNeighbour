9573a5c15d1073abc06eeadc298ae96ae6e09a17
sssbug <59488967+sssbug@users.noreply.github.com>
2022-02-02 04:20:33 +0300

    photo

---
+++ InspectorNeighbor/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //public SnapshotCamera snapCam;
+   
+
+   
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.C))
+    //    {
+    //        snapCam.CallTakeSnapShot();
+    //    }
+    //}
+}
---
+++ InspectorNeighbor/Assets/Scripts/SnapshotCamera.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SnapshotCamera : MonoBehaviour
+{
+    Player player;
+    [Header("Photo Taker")]
+    private Image photoDisplayArea;
+    private Texture2D screenCapture;
+
+    public GameObject game;
+
+    private void Start()
+    {
+        player = GameObject.Find("PlayerArmature").GetComponent<Player>();
+        
+        
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(CapturePhoto());
+        }
+    }
+
+
+    IEnumerator CapturePhoto()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Rect regionToRead = new Rect(0,0,Screen.width,Screen.height);
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenCapture.ReadPixels(regionToRead,0,0,false);
+
+        screenCapture.Apply();
+        
+        ShowPhoto();
+    }
+
+    void ShowPhoto()
+    {
+        
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f,0.0f,screenCapture.width,screenCapture.height), new Vector2(0.5f,0.5f),100.0f);
+
+        GameObject spawn = Instantiate(game,player.transform.position,Quaternion.identity);
+        photoDisplayArea = spawn.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>();
+        
+        photoDisplayArea.sprite = photoSprite;
+    }
+    //    Camera snapCam;
+    //    int resWith = 256;
+    //    int resHeight = 256;
+
+    //    void Awake()
+    //    {
+    //        snapCam = GetComponent<Camera>();
+    //        if (snapCam.targetTexture == null)
+    //        {
+    //            snapCam.targetTexture = new RenderTexture(resWith, resHeight, 24);
+    //        }
+    //        else
+    //        {
+    //            resWith = snapCam.targetTexture.width;
+    //            resHeight = snapCam.targetTexture.height;
+
+    //        }
+    //        snapCam.gameObject.SetActive(false);
+    //    }
+
+
+
+    //    public void CallTakeSnapShot()
+    //    {
+    //        snapCam.gameObject.SetActive(true);
+
+    //    }
+    //    private void LateUpdate()
+    //    {
+    //        if (snapCam.gameObject.activeInHierarchy)
+    //        {
+    //            Texture2D snapshot = new Texture2D(resWith, resHeight, TextureFormat.RGB24, false);
+    //            snapCam.Render();
+    //            RenderTexture.active = snapCam.targetTexture;
+    //            snapshot.ReadPixels(new Rect(0, 0, resWith, resHeight), 0, 0);
+    //            byte[] bytes = snapshot.EncodeToPNG();
+    //            string fileName = SnapShotName();
+    //            System.IO.File.WriteAllBytes(fileName, bytes);
+    //            snapCam.gameObject.SetActive(false);
+    //        }
+    //    }
+
+    //    string SnapShotName()
+    //    {
+    //        return string.Format("{0}/Snapshots/snap_{1}x{2}_{3}.png", Application.dataPath, resWith, resHeight, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    //    }
+}
