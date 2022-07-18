0b2a7615521f4a805bb2f1d1584f8df1c46d69fb
sssbug <59488967+sssbug@users.noreply.github.com>
2022-07-18 05:37:19 +0300

    5:37

--- InspectorNeighbooor/Assets/Scripts/PlayerMove.cs
+++ InspectorNeighbooor/Assets/Scripts/PlayerMove.cs
@@ -28,21 +28,43 @@ public class PlayerMove : MonoBehaviour
 
             if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
             {
+                animator.SetBool("isBack", false);
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", true);
+                animator.SetBool("isWalk", false);
+
                 Vector3 move = transform.right * x + transform.forward * z;
                 variables.controller.Move(move * variables.PlayerSpeed * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
             {
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isBack", false);
+                animator.SetBool("isWalk", false);
+                animator.SetBool("isRight", true);
                 Vector3 move = transform.right * x + transform.forward * z;
                 variables.controller.Move(move * variables.PlayerSpeed * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
             {
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isWalk", false);
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isBack", true);
+                animator.SetBool("isLeft", true);
                 Vector3 move = transform.right * x + transform.forward * z;
                 variables.controller.Move(move * variables.PlayerSpeed * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
             {
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isWalk", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isBack", true);
+                animator.SetBool("isRight", true);
                 Vector3 move = transform.right * x + transform.forward * z;
                 variables.controller.Move(move * variables.PlayerSpeed * Time.deltaTime);
             }
@@ -50,6 +72,8 @@ public class PlayerMove : MonoBehaviour
             {
                 animator.SetBool("isBack", false);
                 animator.SetBool("isIdle", false);
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", false);
                 animator.SetBool("isWalk", true);
 
                 Vector3 move =  transform.forward * z;
@@ -60,6 +84,8 @@ public class PlayerMove : MonoBehaviour
             {
                 animator.SetBool("isIdle", false);
                 animator.SetBool("isWalk", false);
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", false);
                 animator.SetBool("isBack", true);
                 Vector3 move = transform.forward * z;
                 variables.controller.Move(move * variables.PlayerSpeed * Time.deltaTime);
@@ -67,11 +93,21 @@ public class PlayerMove : MonoBehaviour
             }
             else if (Input.GetKey(KeyCode.A))
             {
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isWalk", false);
+                animator.SetBool("isBack", false);
+                animator.SetBool("isRight", false);
+                animator.SetBool("isLeft", true);
                 Vector3 move = transform.right * x;
                 variables.controller.Move(move * variables.PlayerSpeed * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.D))
             {
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isBack", false);
+                animator.SetBool("isWalk", false);
+                animator.SetBool("isRight", true);
                 Vector3 move = transform.right * x;
                 variables.controller.Move(move * variables.PlayerSpeed * Time.deltaTime);
             }
@@ -79,6 +115,8 @@ public class PlayerMove : MonoBehaviour
             {
                 animator.SetBool("isWalk", false);
                 animator.SetBool("isBack", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isRight", false);
                 animator.SetBool("isIdle", true);
                 
                 return;
