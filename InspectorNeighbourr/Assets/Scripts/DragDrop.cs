1d8a3b77a19f2b36ef7712452093b9fcbd2672b3
sssbug <59488967+sssbug@users.noreply.github.com>
2022-07-24 10:00:09 +0300

    7/24/2022

---
+++ InspectorNeighbourr/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private GameObject selectedObject;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (selectedObject == null)
+            {
+                RaycastHit hit = CastRay();
+
+                if (hit.collider != null)
+                {
+                    if (hit.collider.gameObject.layer == 12)
+                    {
+                        selectedObject = hit.collider.gameObject;
+
+
+                    }
+                    else
+                    {
+                        
+                        return;
+                    }
+
+                }
+            }
+            else
+            {
+                Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedObject.transform.position).z);
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+                selectedObject.transform.position = new Vector3(worldPosition.x, 0f, worldPosition.z);
+                selectedObject.GetComponent<Rigidbody>().isKinematic = false;
+                selectedObject.GetComponent<Rigidbody>().useGravity = true;
+                selectedObject = null;
+
+            }
+        }
+
+        if (selectedObject != null)
+        {
+            selectedObject.GetComponent<Rigidbody>().isKinematic = true;
+            selectedObject.GetComponent<Rigidbody>().useGravity = false;
+            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedObject.transform.position).z);
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+            selectedObject.transform.position = new Vector3(worldPosition.x, .25f, worldPosition.z);
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                selectedObject.transform.rotation = Quaternion.Euler(new Vector3(
+                    selectedObject.transform.rotation.eulerAngles.x,
+                    selectedObject.transform.rotation.eulerAngles.y + 90f,
+                    selectedObject.transform.rotation.eulerAngles.z));
+            }
+        }
+    }
+
+    private RaycastHit CastRay()
+    {
+        Vector3 screenMousePosFar = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.farClipPlane);
+        Vector3 screenMousePosNear = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.nearClipPlane);
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+        RaycastHit hit;
+        Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit);
+
+        return hit;
+    }
+
+}
